2627d68310faa2698273caa54dc4da68d58a765b
Łukasz Gołębiewski <lukasz.golebiewski@gmail.com>
2018-07-19 16:54:09 +0200

    Add first draft of LMDBBlockStore

---
+++ block-storage/src/main/scala/coop/rchain/blockstorage/LMDBBlockStore.scala
@@ -0,0 +1,88 @@
+package coop.rchain.blockstorage
+
+import cats._
+import cats.effect.Bracket
+import cats.implicits._
+import cats.mtl.MonadState
+import com.google.protobuf.ByteString
+import coop.rchain.blockstorage.BlockStore.BlockHash
+import coop.rchain.casper.protocol.BlockMessage
+import coop.rchain.metrics.Metrics
+
+import scala.language.higherKinds
+import scala.collection.JavaConverters._
+
+import java.nio.ByteBuffer
+import java.nio.file.Path
+import org.lmdbjava.DbiFlags.MDB_CREATE
+import org.lmdbjava._
+
+class LMDBBlockStore[F[_]] private (val env: Env[ByteBuffer], path: Path, blocks: Dbi[ByteBuffer])(
+    implicit
+    val applicative: Applicative[F],
+    bracketF: Bracket[F, Exception],
+    metricsF: Metrics[F])
+    extends BlockStore[F] {
+
+  implicit class RichBlockHash(byteVector: BlockHash) {
+
+    def toDirectByteBuffer: ByteBuffer = {
+      val buffer: ByteBuffer = ByteBuffer.allocateDirect(byteVector.size)
+      byteVector.copyTo(buffer)
+      // TODO: get rid of this:
+      buffer.flip()
+      buffer
+    }
+  }
+
+  def put(blockHash: BlockHash, blockMessage: BlockMessage): F[Unit] =
+    for {
+      _ <- metricsF.incrementCounter("block-store-put")
+      ret <- bracketF.bracket(applicative.pure(env.txnWrite()))(txn =>
+              applicative.pure {
+                blocks.put(txn,
+                           blockHash.toDirectByteBuffer,
+                           blockMessage.toByteString.toDirectByteBuffer)
+                txn.commit()
+            })(txn => applicative.pure(txn.close()))
+    } yield ret
+
+  def get(blockHash: BlockHash): F[Option[BlockMessage]] =
+    for {
+      _ <- metricsF.incrementCounter("block-store-get")
+      ret <- bracketF.bracket(applicative.pure(env.txnRead()))(txn =>
+              applicative.pure {
+                val r = Option(blocks.get(txn, blockHash.toDirectByteBuffer)).map(r =>
+                  BlockMessage.parseFrom(ByteString.copyFrom(r).newCodedInput()))
+                txn.commit()
+                r
+            })(txn => applicative.pure(txn.close()))
+    } yield ret
+
+  def asMap(): F[Map[BlockHash, BlockMessage]] =
+    for {
+      _ <- metricsF.incrementCounter("block-store-as-map")
+      ret <- bracketF.bracket(applicative.pure(env.txnRead()))(txn =>
+              applicative.pure {
+                val r = blocks.iterate(txn).asScala.foldLeft(Map.empty[BlockHash, BlockMessage]) {
+                  (acc: Map[BlockHash, BlockMessage], x: CursorIterator.KeyVal[ByteBuffer]) =>
+                    val hash = ByteString.copyFrom(x.key())
+                    val msg  = BlockMessage.parseFrom(ByteString.copyFrom(x.`val`()).newCodedInput())
+                    acc.updated(hash, msg)
+                }
+                txn.commit()
+                r
+            })(txn => applicative.pure(txn.close()))
+    } yield ret
+
+}
+
+object LMDBBlockStore {
+  def create[F[_]](env: Env[ByteBuffer], path: Path)(implicit
+                                                     bracketF: Bracket[F, Exception],
+                                                     metricsF: Metrics[F]): BlockStore[F] = {
+
+    val blocks: Dbi[ByteBuffer] = env.openDbi(s"blocks", MDB_CREATE)
+    new LMDBBlockStore(env, path, blocks)
+  }
+}
--- block-storage/src/test/scala/coop/rchain/blockstorage/BlockStoreTest.scala
+++ block-storage/src/test/scala/coop/rchain/blockstorage/BlockStoreTest.scala
@@ -14,24 +14,65 @@ import org.scalatest.prop.GeneratorDrivenPropertyChecks
 
 import scala.language.higherKinds
 
-class BlockStoreTest
-    extends FlatSpec
+trait BlockStoreTest
+    extends FlatSpecLike
     with Matchers
     with OptionValues
     with GeneratorDrivenPropertyChecks
-    with BeforeAndAfterEach {
+    with BeforeAndAfterEach
+    with BeforeAndAfterAll {
 
   implicit override val generatorDrivenConfig: PropertyCheckConfiguration =
     PropertyCheckConfiguration(minSuccessful = PosInt(100))
 
-  override def beforeEach() {}
+  override def beforeEach(): Unit = {}
 
-  override def afterEach() {}
+  override def afterEach(): Unit = {}
 
   def bm(v: Long, ts: Long): BlockMessage =
     BlockMessage().withHeader(Header().withVersion(v).withTimestamp(ts))
 
-  def withStore[R](f: BlockStore[Id] => R): R = {
+  def withStore[R](f: BlockStore[Id] => R): R
+
+  //TODO make generative
+  "Block Store" should "return None on get while it's empty" in withStore { store =>
+    val key: BlockHash = ByteString.copyFrom("testkey", "utf-8")
+    store.get(key) shouldBe None
+  }
+
+  //TODO make generative
+  "Block Store" should "return Some(message) on get for a published key" in withStore { store =>
+    val items = 0 to 100 map { i =>
+      val key: BlockHash    = ByteString.copyFrom("testkey" + i, "utf-8")
+      val msg: BlockMessage = bm(100L + i, 10000L + i)
+      (key, msg)
+    }
+    items.foreach { case (k, v) => store.put(k, v) }
+    items.foreach {
+      case (k, v) =>
+        store.get(k) shouldBe Some(v)
+    }
+    store.asMap() should have size items.size
+  }
+
+  "Block Store" should "overwrite existing value" in withStore { store =>
+    val items = 0 to 100 map { i =>
+      val key: BlockHash     = ByteString.copyFrom("testkey" + i, "utf-8")
+      val msg1: BlockMessage = bm(100L + i, 10000L + i)
+      val msg2: BlockMessage = bm(200L + i, 20000L + i)
+      (key, msg1, msg2)
+    }
+    items.foreach { case (k, v1, _) => store.put(k, v1) }
+    items.foreach { case (k, v1, _) => store.get(k) shouldBe Some(v1) }
+    items.foreach { case (k, _, v2) => store.put(k, v2) }
+    items.foreach { case (k, _, v2) => store.get(k) shouldBe Some(v2) }
+
+    store.asMap() should have size items.size
+  }
+}
+
+class InMemBlockStoreTest extends BlockStoreTest {
+  override def withStore[R](f: BlockStore[Id] => R): R = {
 
     implicit val bracket: Bracket[Id, Exception] =
       new Bracket[Id, Exception] {
@@ -66,40 +107,86 @@ class BlockStoreTest
     val store = BlockStore.createMapBased[Id]
     f(store)
   }
+}
 
-  //TODO make generative
-  "Block Store" should "return None on get while it's empty" in withStore { store =>
-    val key: BlockHash = ByteString.copyFrom("testkey", "utf-8")
-    store.get(key) shouldBe None
+import java.nio.file.{Files, Path}
+import org.lmdbjava._
+
+class LMDBBlockStoreTest extends BlockStoreTest {
+
+  val dbDir: Path = Files.createTempDirectory("block-store-test-")
+
+  override def withStore[R](f: BlockStore[Id] => R): R = {
+
+    implicit val bracket: Bracket[Id, Exception] =
+      new Bracket[Id, Exception] {
+        def pure[A](x: A): cats.Id[A] = implicitly[Applicative[Id]].pure(x)
+
+        // Members declared in cats.ApplicativeError
+        def handleErrorWith[A](fa: cats.Id[A])(f: Exception => cats.Id[A]): cats.Id[A] =
+          ??? //implicitly[ApplicativeError[Id, Exception]].handleErrorWith(fa)(f)
+
+        def raiseError[A](e: Exception): cats.Id[A] = ???
+        //implicitly[ApplicativeError[Id, Exception]].raiseError(e)
+
+        // Members declared in cats.FlatMap
+        def flatMap[A, B](fa: cats.Id[A])(f: A => cats.Id[B]): cats.Id[B] =
+          implicitly[FlatMap[Id]].flatMap(fa)(f)
+        def tailRecM[A, B](a: A)(f: A => cats.Id[Either[A, B]]): cats.Id[B] =
+          implicitly[FlatMap[Id]].tailRecM(a)(f)
+
+        def bracketCase[A, B](acquire: A)(use: A => B)(
+            release: (A, ExitCase[Exception]) => Unit): B = {
+          val state = acquire
+          try {
+            use(state)
+          } finally {
+            release(acquire, ExitCase.Completed)
+          }
+        }
+
+      }
+
+    implicit val metrics: Metrics[Id] = new MetricsNOP[Id]()(bracket)
+
+    val mapSize: Long = 1024L * 1024L * 4096L
+    val env = Env
+      .create()
+      .setMapSize(mapSize)
+      .setMaxDbs(1)
+      .setMaxReaders(8)
+      .open(dbDir.toFile, List(EnvFlags.MDB_NOTLS): _*)
+
+    try {
+      val store = LMDBBlockStore.create[Id](env, dbDir)
+      f(store)
+    } finally {
+      env.close()
+    }
   }
 
-  //TODO make generative
-  "Block Store" should "return Some(message) on get for a published key" in withStore { store =>
-    val items = 0 to 100 map { i =>
-      val key: BlockHash    = ByteString.copyFrom("testkey" + i, "utf-8")
-      val msg: BlockMessage = bm(100L + i, 10000L + i)
-      (key, msg)
-    }
-    items.foreach { case (k, v) => store.put(k, v) }
-    items.foreach {
-      case (k, v) =>
-        store.get(k) shouldBe Some(v)
-    }
-    store.getAll() should have size items.size
+  override def afterAll(): Unit = {
+    recursivelyDeletePath(dbDir)
+    super.afterAll()
   }
 
-  "Block Store" should "overwrite existing value" in withStore { store =>
-    val items = 0 to 100 map { i =>
-      val key: BlockHash     = ByteString.copyFrom("testkey" + i, "utf-8")
-      val msg1: BlockMessage = bm(100L + i, 10000L + i)
-      val msg2: BlockMessage = bm(200L + i, 20000L + i)
-      (key, msg1, msg2)
-    }
-    items.foreach { case (k, v1, _) => store.put(k, v1) }
-    items.foreach { case (k, v1, _) => store.get(k) shouldBe Some(v1) }
-    items.foreach { case (k, _, v2) => store.put(k, v2) }
-    items.foreach { case (k, _, v2) => store.get(k) shouldBe Some(v2) }
+  import java.nio.ByteBuffer
+  import java.nio.file.attribute.BasicFileAttributes
+  import java.nio.file.{FileVisitResult, Files, Path, SimpleFileVisitor}
 
-    store.getAll() should have size items.size
-  }
+  private def makeDeleteFileVisitor: SimpleFileVisitor[Path] =
+    new SimpleFileVisitor[Path] {
+      override def visitFile(p: Path, attrs: BasicFileAttributes): FileVisitResult = {
+        Files.delete(p)
+        FileVisitResult.CONTINUE
+      }
+      override def postVisitDirectory(p: Path, e: java.io.IOException): FileVisitResult = {
+        Files.delete(p)
+        FileVisitResult.CONTINUE
+      }
+    }
+
+  def recursivelyDeletePath(p: Path): Path =
+    Files.walkFileTree(p, makeDeleteFileVisitor)
+
 }
